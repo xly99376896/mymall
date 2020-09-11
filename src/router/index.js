948917e5eb6255301c1ff956cd32be4c829fa0f4
xly993768967 <993768967@qq.com>
2020-09-11 16:09:25 +0800

    动态修改标题

--- src/router/index.js
+++ src/router/index.js
@@ -11,28 +11,46 @@ Vue.use(VueRouter)
   const routes = [
   {
       path: '',
-      redirect: '/home'
+      redirect: '/home',
+      meta: {
+        title: '首页'
+      }
   },
   {
       path: '/home',
-      component: Home
+      component: Home,
+      meta: {
+        title: '首页'
+      }
   },
   {
       path: '/category',
-      component: Category
+      component: Category,
+      meta: {
+        title: '分类'
+      }
   },
   {
       path: '/cart',
-      component: Cart
+      component: Cart,
+      meta: {
+        title: '购物车'
+      }
   },
   {
       path: '/profile',
-      component: Profile
+      component: Profile,
+      meta: {
+        title: '我的'
+      }
   },
   {
     // path: '/detail/:iid',
       path: '/detail/',
-      component: Detail
+      component: Detail,
+      meta: {
+        title: '详情'
+      }
   },
 ]
 
@@ -40,6 +58,10 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
+})
+
+router.afterEach((to, from) => {
+  document.title = to.matched[0].meta.title
 })
 
 export default router
